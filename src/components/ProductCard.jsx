c15240a0b7ee6d4954432e80868f2d7fba0f7939
qwe7fd <porvamaksim7@gmail.com>
2026-03-20 14:01:15 +0300

    feat: add products serive and mock data

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,10 @@ const ProductCard = () => {
       className="block rounded-lg border border-gray-200 bg-white hover:shadow-lg"
       href="#"
     >
-      
+      <div className="aspect-square overflow-hidden bg-gray-100"></div>
+      <div className="p-4">
+        <h3 className="mb-2"></h3>
+      </div>
     </a>
   )
 }
