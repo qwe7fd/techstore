45f230affb58a18d86cf34249ba333287b9c7dc1
qwe7fd <porvamaksim7@gmail.com>
2026-03-26 10:22:40 +0300

    feat: add link to product page

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+
+
+const ProductDetails = () => {
+  <main className="flex-1">
+    <div className="bg-gray-50 min-h-screen container mx-auto px-4 lg:px-8 py-8">
+      
+    </div>
+  </main>
+}
+
+export default ProductDetails;
